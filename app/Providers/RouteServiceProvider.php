41f355458327d25f801d09cc2e413a9339f70a34
Mileshkumar Vaghela <milesh2309@gmail.com>
2025-11-27 14:44:57 +0530

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,11 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+    
     public function boot()
     {
         $this->routes(function () {
